9b17cfa0c94cc14aa68c1776f4fb10e8b26a0239
Alex <alex@straddle.io>
2024-09-10 10:29:01 +0100

    chore(core): add initial files

---
+++ packages/bridge-core/src/types.ts
@@ -0,0 +1,13 @@
+export enum TMessageToJSBridgeType {
+    PING = '@straddleio/js-bridge/ping',
+    ERROR = '@straddleio/js-bridge/error',
+    INITIALIZE = '@straddleio/js-bridge/initialize',
+    INITIALIZING = '@straddleio/js-bridge/initializing',
+    INITIALIZED = '@straddleio/js-bridge/initialized',
+    MOUNTED = '@straddleio/js-bridge/mounted',
+    ON_WALLET_TOKEN = '@straddleio/js-bridge/on-wallet-token',
+    ON_SUCCESS = '@straddleio/js-bridge/on-success',
+    ON_SUCCESS_CTA_CLICKED = '@straddleio/js-bridge/on-success-cta-clicked',
+    TOKEN = '@straddleio/js-bridge/token',
+    DEBUG = '@straddleio/js-bridge/debug',
+}
