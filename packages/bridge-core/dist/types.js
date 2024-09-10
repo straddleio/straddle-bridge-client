47816a544bcf456810ea94cb02fd03679f53eacf
Alex <alex@straddle.io>
2024-09-10 10:31:54 +0100

    chore(core): add dist folder

---
+++ packages/bridge-core/dist/types.js
@@ -0,0 +1,14 @@
+export var TMessageToJSBridgeType;
+(function (TMessageToJSBridgeType) {
+    TMessageToJSBridgeType["PING"] = "@straddleio/js-bridge/ping";
+    TMessageToJSBridgeType["ERROR"] = "@straddleio/js-bridge/error";
+    TMessageToJSBridgeType["INITIALIZE"] = "@straddleio/js-bridge/initialize";
+    TMessageToJSBridgeType["INITIALIZING"] = "@straddleio/js-bridge/initializing";
+    TMessageToJSBridgeType["INITIALIZED"] = "@straddleio/js-bridge/initialized";
+    TMessageToJSBridgeType["MOUNTED"] = "@straddleio/js-bridge/mounted";
+    TMessageToJSBridgeType["ON_WALLET_TOKEN"] = "@straddleio/js-bridge/on-wallet-token";
+    TMessageToJSBridgeType["ON_SUCCESS"] = "@straddleio/js-bridge/on-success";
+    TMessageToJSBridgeType["ON_SUCCESS_CTA_CLICKED"] = "@straddleio/js-bridge/on-success-cta-clicked";
+    TMessageToJSBridgeType["TOKEN"] = "@straddleio/js-bridge/token";
+    TMessageToJSBridgeType["DEBUG"] = "@straddleio/js-bridge/debug";
+})(TMessageToJSBridgeType || (TMessageToJSBridgeType = {}));
